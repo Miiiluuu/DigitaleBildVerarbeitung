c83472ae7138945a4d40d9eeac90e66bed09730b
Mieke <mieke.luisa@freenet.de>
2018-12-19 18:57:43 +0100

    19.12.18 Beginn 3.9
    3.8 wurde beendet

--- Aufgabe 1_1/Aufgabe_3_8.py
+++ Aufgabe 1_1/Aufgabe_3_8.py
@@ -8,133 +8,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import Aufgabe_1_1
-import Aufgabe_3_4
-import Aufgabe_3_3
 import Aufgabe_3_7
 
 
-def make_logic_image(image, schwelle_oben):
-    """ Funktion erzeugt aus einem Bild ein logisches Bild (Binaerbild), indem
-        ein bestimmter Bereich mittels des Schwellwertverfahrens separiert
-        wird. Dabei werden a priori Kenntnisse vorausgesetzt
-        (Grauwerthistogramm u.Ä.). Beinhaltet Pixelklassifikation: Einteilung
-        in Objekt- und Nichtobjektpixel.
-
-        Parameter:
-        ----------
-        image: Array, Eingabewerte.
-
-        schwelle_oben: oberer Schwellwert. Bis zu dieser Graustufe wird
-        Grauwertverteilung als Objekt gezaehlt.
-    """
-    # Anlegen eines Null-Arrays, welches Maske enthaelt:
-    # Einsen: Pixel wird als Objekt klassifiziert
-    # Nullen: Pixel wird als Nichtobjekt klassifiziert
-    image_maske = np.zeros_like(image)
-    image_maske[schwelle_oben <= image] = 1
-    return image_maske
-
-
-def extract_values(image, value):
-    """ Funktion speichert aus einem Array die einander zugehoerigen (x- und
-         y-)Koordinaten (als Meshgrid), welche einen bestimmten Wert 'value'
-         enthalten. Dabei wird der (virtuelle) Ursprung in das Zentrum des
-         Bildes gelegt.
-
-        Parameter:
-        ----------
-        image: Array, Eingabewerte.
-
-        value: bestimmter Wert, anhand dessen Filterung der Werte.
-    """
-    # Zentrum des Bildes
-    center = len(image) // 2
-    # Abspeichern der einander zugehoerigen Koordinaten
-    koord_x, koord_y = np.meshgrid(np.arange(-center, center),
-                                   np.arange(center, -center, step=-1))
-    # Rausfilterung der Untergrundwerte nach einen bestimmten Wert 'value'
-    koord_x = koord_x[image == value]
-    koord_y = koord_y[image == value]
-    return koord_x, koord_y
-
-
-def hough_trafo(koord_x, koord_y):
-    """ Anwenden der Hough-Transformation = Kantenorientierte Segmentierung.
-        Mittels der Hesseschen Normalform fuer Geradengleichungen werden
-        Geraden erkannt (siehe Vorlesung zu Modul MF-MRS_14 Digitale
-        Bildverarbeitung, Folie 202f). Dafuer wird der Normalenvektor vom
-        (virtuellen) Ursprung des Bildes und der zugehoerige Winkel
-        parametrisiert.
-
-        Parameter:
-        ----------
-        koord_x: (x-)Koordinaten einer Bildfunktion f(x, y), fuer die gilt:
-            f(x, y) ungleich 0 .
-
-        koord_y: (y-)Koordinaten einer Bildfunktion f(x, y), fuer die gilt:
-            f(x, y) ungleich 0 .
-    """
-    winkel = []
-    abstaende = []
-    # verschiedene Winkel durchgehen
-    for alpha in np.linspace(0, 180, 180, endpoint=False):
-        # einzelne aktuelle Winkel abspeichern (Anzahl entsprechend Anzahl
-        # der Koordinaten)
-        winkel.append(np.ones(len(koord_x))*alpha)
-        # Winkel in Bogenmaß umrechen
-        alpha = np.radians(alpha)
-        # Hessesche Normalform der Geradengleichung
-        abstand = koord_x * np.cos(alpha) + koord_y * np.sin(alpha)
-        # zu aktuellen Winkel alle dazu berechneten Abstaende abspeichern
-        abstaende.append(abstand)
-    # Umwandeln der Listen in Arrays
-    winkel = np.array(winkel).ravel()
-    abstaende = np.array(abstaende).ravel()
-    # Darstellung der verketteten Kanten / durchgezogenen Linien des Dreieckes
-    # als Häufungspunkte im 2D-Histogramm
-    plot_2d_hist("Kantenorientierte Segmentierung \n"
-                 "- Hough-Transformation zur Erkennung der Seiten des \n"
-                 "Dreieckes aus Flaechenquelle D, Aufgabe 1.1 -",
-                 r'Winkel $ϕ/°$', r'Abstand zum Mittelpunkt $d/mm$', winkel,
-                 abstaende)
-
-
-def plot_2d_hist(ueberschrift, abszisse, ordinate, werte1, werte2):
-    """ Plot einer 2D-Histogramms mit entsprechender Ueberschrift,
-        Achsenbeschriftung, Farbskala etc.
-    """
-    plt.figure(figsize=(10, 6))
-    # Hinzufuegen der Ueberschrift zum Plot
-    plt.suptitle(ueberschrift, fontsize=16)
-    # Achsenbeschriftungen
-    plt.xlabel(abszisse)
-    plt.ylabel(ordinate)
-    counts, xedges, yedges, image = plt.hist2d(werte1, werte2, bins=180,
-                                               range=[[0, 180], [-64, 64]])
-    # Ueberlappungen vermeiden
-    plt.tight_layout(rect=[0, 0.03, 1, 0.8])
-    plt.colorbar(image)
-    plt.show()
-
-
-def calc_schwerpkt(image):
+def calc_schwerpkt_1d(image):
     """ Funktion berechnet bestimmte Momente von Bildern und darauf aufbauend
-        den Schwerpunkt dieses Bildes entsprechend Vorlesung zum Modul
-        MF-MRS_14 Digitale Bildverarbeitung, Folie 213f).
+        (in einer Dimension) den Schwerpunkt dieses Bildes entsprechend
+        Vorlesung zum Modul  MF-MRS_14 Digitale Bildverarbeitung, Folie 213f.
 
         Parameter:
         ----------
         image: Array, Eingabewerte.
     """
     moment0 = np.sum(image)
-    moment = 0
-    schleife = np.shape(image)[1]
-    for x in range(schleife):
-        moment += np.sum(image[:, x]) * x
-    # in x-Richtung
+    x_koord = np.arange(np.shape(image)[1])
+    moment = np.sum(image, axis=0) * x_koord
+    moment = np.sum(moment)
+    # Schwerpunkt in 1D
     schwerpkt = moment / moment0
     return schwerpkt
-    
+
+
+def calc_schwerpkt(image, calc_geometric=False):
+    """ Funktion berechnet den Schwerpunkt eines Bildes entsprechend
+        Vorlesung zum Modul  MF-MRS_14 Digitale Bildverarbeitung, Folie 213f.
+
+        Parameter:
+        ----------
+        image: Array, Eingabewerte.
+    """
+    # Berechnung vom geometrischen Schwerpunkt falls Bedingung zutrifft
+    if calc_geometric:
+        # dafuer wird zuvor zunaechst logisches Bild erzeugt:
+        # Bildfunktion wird ueberall auf Eins gesetzt
+        image = Aufgabe_3_7.make_logic_image(image, 0)
+    # Berechnung vom Schwerpunkt in x-Richtung
+    schwerpkt_x = calc_schwerpkt_1d(image)
+    # Berechnung vom Schwerpunkt in y-Richtung
+    image_t = np.transpose(image)
+    schwerpkt_y = calc_schwerpkt_1d(image_t)
+    return schwerpkt_x, schwerpkt_y
+
+
+def plot_lines(ueberschrift, image, schnittpkt):
+    """ Funktion erstellt einen Plot mit Ueberschriften, Achsenbeschriftungen
+        und Ähnliches. Es wird ein Bild geplottet mit einem Schnittpunkt
+        bestimmter Geraden.
+
+        Parameter:
+        ----------
+        image: Array, Eingabewerte.
+
+        schnittpkt: enthält Koordinaten (in x- und y-Richtung), die einen
+            Schnittpunkt im geplotten Bild darstellen.
+    """
+    plt.figure()
+    # Hinzufuegen der Ueberschrift zum Plot
+    plt.suptitle(ueberschrift, fontsize=16)
+    plt.imshow(image, cmap='gray')
+    # x-Koordinate als Vertikale plotten
+    plt.axvline(schnittpkt[0], color="deeppink")
+    # y-Koordinate als Horizontale plotten
+    plt.axhline(schnittpkt[1], color="deeppink")
+    # Ueberlappungen vermeiden
+    plt.tight_layout(rect=[0, 0.03, 1, 0.85])
+
 
 def main():
     # Bild- Array (aus Aufgabe 1.1) erstellen
@@ -142,72 +80,17 @@ def main():
     # Einteilung Bild aus Aufgabe 1.1 in Teilbilder:
     # Extraktion des rechten unteren Quadranten
     szinti = Aufgabe_1_1.extract(szinti, 0, 256, 0, 128)
-    # Berechnung von geometrischen Schwerpunkt in x-Richtung
-    schwerpkt_masse_x = calc_schwerpkt(szinti)
-    # Berechnung von Massenschwerpunkt in y-Richtung
-    szinti_t = np.transpose(szinti)
-    schwerpkt_masse_y = calc_schwerpkt(szinti_t)
-    # Berechnung vom geometrischen Schwerpunkt
-    # dafuer wird zuvor zunaechst logisches Bild erzeugt:
-    # Bildfunktion wird ueberall auf Eins gesetzt
-    szinti_geo = Aufgabe_3_7.make_logic_image(szinti, 0)
-    # geometrischer Schwerpunkt in x-Richtung
-    schwerpkt_geo_x = calc_schwerpkt(szinti_geo)
-    # Berechnung von Massenschwerpunkt in y-Richtung
-    szinti_geo_t = np.transpose(szinti_geo_t)
-    schwerpkt_geo_y = calc_schwerpkt(szinti_geo_t)
-    
-#    # momente berechnen
-#    m00 = np.sum(szinti)
-#    m10 = 0
-#    schleife = np.shape(szinti)[1]
-#    for x in range(schleife):
-#        m10 += np.sum(szinti[:, x]) * x
-#    szinti_t = np.transpose(szinti)
-#    schleife_t = np.shape(szinti_t)[1]
-#    m01 = 0
-#    for x in range(schleife_t):
-#        m01 += np.sum(szinti_t[:, x]) * x
-#    # Rechnung Schwerpunkte nach Vorlesung zu Modul MF-MRS_14 Digitale
-#    # Bildverarbeitung Folie 213f
-#    # Massenschwerpunkt in x-Richtung
-#    x_s = m10 /m00
-#    # Massenschwerpunkt in y-Richtung
-#    y_s = m01 /m00
-#    # Maskieren
-#    # geometrischer Schwerpunkt wird entsprechend berechnet, aber zuvor wird Bild-
-#    # funktion ueberall auf Eins gesetzt
-#    # Schwellwertbedingung: in welchem Intervall liegen Objektpixel?
-#    szinti_geo = Aufgabe_3_7.make_logic_image(szinti, 0)
-#    # momente berechnen
-#    m00_geo = np.sum(szinti_geo)
-#    m10_geo = 0
-#    schleife_geo = np.shape(szinti_geo)[1]
-#    for x in range(schleife_geo):
-#        m10_geo += np.sum(szinti_geo[:, x]) * x
-#    szinti_t_geo = np.transpose(szinti_geo)
-#    schleife_t_geo = np.shape(szinti_t_geo)[1]
-#    m01_geo = 0
-#    for x in range(schleife_t_geo):
-#        m01_geo += np.sum(szinti_t_geo[:, x]) * x
-#    # Rechnung Schwerpunkte nach Vorlesung zu Modul MF-MRS_14 Digitale
-#    # Bildverarbeitung Folie 213f
-#    # Massenschwerpunkt in x-Richtung
-#    x_geo = m10_geo /m00_geo
-#    # Massenschwerpunkt in y-Richtung
-#    y_geo = m01_geo /m00_geo
-#    # Einzeichnen des geometrischen Schwerpunktes
-#    plt.figure()
-#    plt.imshow(szinti, cmap='gray')
-#    plt.axhline(x_s, color="deeppink", linestyle="dashed")
-#    plt.axvline(y_s, color="deeppink", linestyle="dashed")
-#    # Einzeichnen des Massenschwerpunktes
-#    plt.figure()
-#    plt.imshow(szinti, cmap='gray')
-#    plt.axhline(x_geo, color="deeppink", linestyle="dashed")
-#    # Ueberlebenswahrscheinlichkeit nach 1 Jahr, 2 und 5 Jahren
-#    # (1 Jahr ≙ 12 Monate)
-#    plt.axvline(y_geo, color="deeppink", linestyle="dashed")
+    # Berechnung vom geometrischen Schwerpunkt (in x- und y-Richtung)
+    schwerpkt_geo = calc_schwerpkt(szinti, calc_geometric=True)
+    # Berechnung vom Massenschwerpunkt (in x- und y-Richtung)
+    schwerpkt_mass = calc_schwerpkt(szinti)
+    # Einzeichnen des geometrischen Schwerpunktes
+    plot_lines("geometrischer Schwerpunkt \n"
+               "- innerhalb Bild aus Aufgabe 1.1 -", szinti, schwerpkt_geo)
+    # Einzeichnen des Massenschwerpunktes
+    plot_lines("Massenschwerpunkt \n"
+               "- innerhalb Bild aus Aufgabe 1.1 -", szinti, schwerpkt_mass)
+
 
 if __name__ == "__main__":
     main()
---
+++ Aufgabe 1_1/Aufgabe_3_9.py
@@ -0,0 +1,74 @@
+"""
+    Aufgabe 3.9:
+    Extraktion von Flaechenquelle B aus Szintigramm Aufgabe 1.1,
+    Bestimmung der Grauwertuebergangsmatrix C(δ=(1,0)) und C(δ=(0,1)) und
+    Interpretation.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import Aufgabe_1_1
+import Aufgabe_3_7
+import Aufgabe_3_3
+
+def medianfilter_5x5(image):
+    """ Anwendung eines 5x5-Medianfilters auf ein Bild 'image'.
+
+        Parameter:
+        ----------
+        image: Array, Eingabewerte.
+    """
+    # Schleife: jeden Pixel einzeln durchgehen (bis auf aeußersten Pixel
+    # (-Rand)), da dieser von Filter nicht beruecksichtigt wird:
+    # aeußeren Rand-Pixel werden auf Null gesetzt
+    image_gefiltert = np.zeros((len(image), len(image)))
+    for x in range(2, len(image)-2):
+        for y in range(2, len(image)-2):
+            # Filterbereich, der einzeln (fuer jeden Pixel) wirksam wird (5x5)
+            bereich = image[y-2:y+3, x-2:x+3]
+            # Anwenden eines Medianfilters auf das Bild
+            image_gefiltert[y, x] = np.median(bereich)
+    return image_gefiltert
+
+
+def main():
+    # Bild- Array (aus Aufgabe 1.1) erstellen
+    szinti, pixel, pixel_quadrant = Aufgabe_1_1.make_szinti()
+    # Einteilung Bild aus Aufgabe 1.1 in Teilbilder:
+    # Extraktion des linken oberen Quadranten (Flaechenquelle B)
+    szinti = Aufgabe_1_1.extract(szinti, 0, 128, 0, 128)
+    # Kontrolldarstellung
+    plt.figure()
+    # Hinzufuegen der Ueberschrift zum Plot
+    plt.suptitle("Originalbild", fontsize=16)
+    plt.imshow(szinti, cmap='gray')
+    # mehrmalige Anwendung von Medianfiltern (verschiedener Groeße), um
+    # Bildrauschen (durch radioaktivem Zerfall) zu reduzieren, aber Lage
+    # und Steilheit vom Bild erhalten!
+    # zweimaliges Anwenden eines 5x5 Medianfilters
+    for i in range(2):
+        szinti = medianfilter_5x5(szinti)
+    # zweimaliges Anwenden eines 3x3 Medianfilters
+    for i in range(2):
+        szinti = Aufgabe_3_3.filter_image(szinti)
+    # Kontrolldarstellung
+    plt.figure()
+    # Hinzufuegen der Ueberschrift zum Plot
+    plt.suptitle("gefiltertes Bild", fontsize=16)
+    plt.imshow(szinti, cmap='gray')
+
+    
+    
+    
+        
+    
+    
+
+    
+
+
+if __name__ == "__main__":
+    main()
+
+# TDO: externe Console bzw Windowkonsole funktioniert nicht?
